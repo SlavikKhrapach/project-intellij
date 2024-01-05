e5f73b4e745ea263a18cd2b866580f768733aebf
SlavikKhrapach <slavikkhrapach@gmail.com>
2024-01-04 22:08:06 -0800

    Added a method to print a header.

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,17 @@
 public class Main {
     public static void main(String[] args) {
+        printHeader();
         System.out.println("Hello world!");
+
+        printHeader();
         System.out.println("Author: Slavik");
-        printMessage("Hello");
     }
 
     private static void printMessage(String message) {
         System.out.println(message);
+    }
+
+    private static void printHeader() {
+        System.out.println("****************************************");
     }
 }
