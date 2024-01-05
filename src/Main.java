91ddff0b9cfbd20043728a93319ab84d5f24c657
SlavikKhrapach <slavikkhrapach@gmail.com>
2024-01-04 21:17:36 -0800

    Refactored my Main class.

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,15 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Author: Slavik");
+        printHello();
+    }
+
+    private static void printMessage(String message) {
+        System.out.println(message);
+    }
+
+    private static void printHello() {
+        printMessage("Hello");
+
     }
 }
